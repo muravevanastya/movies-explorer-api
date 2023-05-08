71ec79f22bc59ec35612dc4ebdebcbd9f614e127
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-08 20:09:58 +0300

    создала контроллеры для фильмов

---
+++ contollers/movies.js
@@ -0,0 +1,78 @@
+const Movie = require('../models/movie');
+const BadRequest = require('../errors/BadRequest');
+const NotFound = require('../errors/NotFound');
+const Forbidden = require('../errors/Forbidden');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({})
+    .populate('owner')
+    .then((movies) => res.send(movies))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+
+  const { _id } = req.user;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailer,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: _id,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .populate(['owner', 'likes'])
+    .orFail(() => {
+      throw new NotFound('Фильм с таким id не найдена');
+    })
+    .then((movie) => {
+      if (movie.owner._id.toString() === req.user._id) {
+        Movie.findByIdAndRemove(movieId)
+          .then((movieData) => res.send(movieData))
+          .catch(next);
+      } else {
+        throw new Forbidden('Недостаточно прав');
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequest('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
+};
