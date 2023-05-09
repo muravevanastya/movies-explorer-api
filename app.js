54c68d509e204be66538c1344d20dc738f6a4da3
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-09 16:31:46 +0300

    добавила роуты в app.js

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
+const NotFound = require('./errors/NotFound');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
+
+app.use('/', require('./routes/auth'));
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+app.use('/movies', require('./routes/movies'));
+
+app.use('*', (req, res, next) => {
+  next(new NotFound('Адреса по вашему запросу не существует'));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+  next();
+});
 
 app.listen(PORT);
