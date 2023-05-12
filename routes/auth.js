918eac33ab389627c4386383d8a4f7b24657782c
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-05-12 11:16:26 +0300

    реализовала валидацию

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { login, createUser } = require('../contollers/users');
 
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getCurrentUserInfo } = require('../contollers/users');
 const { updateCurrentUserInfo } = require('../contollers/users');
 
 router.get('/me', getCurrentUserInfo);
-router.patch('/me', updateCurrentUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+  }),
+}), updateCurrentUserInfo);
 
 module.exports = router;
