df7e6e120f442ea11aa9aabb28b12f24e4445daf
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-06-21 19:36:04 +0300

    настроила cors

--- middlewares/corsValidator.js
+++ middlewares/corsValidator.js
@@ -20,7 +20,7 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    res.status(200).send();
     return;
   }
   next();
