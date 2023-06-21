540016fa51936c3b7e91c1bedbca55e2aef433f0
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-06-21 19:20:20 +0300

    настроила cors

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
 const auth = require('./middlewares/auth');
 const NotFound = require('./errors/NotFound');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const corsValidator = require('./middlewares/corsValidator');
 const limiter = require('./middlewares/rateLimiter');
 
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
@@ -15,34 +15,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// app.use(cors());
-const allowedCors = [
-  'http://moviesearch.nomoredomains.monster',
-  'https://moviesearch.nomoredomains.monster',
-  'https://api.moviesearch.nomoredomains.monster',
-  'http://api.moviesearch.nomoredomains.monster',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'http://localhost:3001',
-  'https://localhost:3001',
-];
-
-module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-    return;
-  }
-  next();
-};
+app.use(corsValidator);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
---
+++ middlewares/corsValidator.js
@@ -0,0 +1,27 @@
+const allowedCors = [
+  'http://moviesearch.nomoredomains.monster',
+  'https://moviesearch.nomoredomains.monster',
+  'https://api.moviesearch.nomoredomains.monster',
+  'http://api.moviesearch.nomoredomains.monster',
+  'http://localhost:3000',
+  'https://localhost:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+    return;
+  }
+  next();
+};
